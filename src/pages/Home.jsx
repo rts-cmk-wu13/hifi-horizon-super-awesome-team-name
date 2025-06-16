eb695b6c661b3b43ef381b122a9f1a7d01ca0730
broto1234 <satyo97du@gmail.com>
2025-06-16 09:14:33 +0200

    added Hero image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router"
+import Hero from "../components/hero/Hero";
 
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
-            <Link to="/contact">Contact</Link>
+            <Hero />
         </>
     )
 }
