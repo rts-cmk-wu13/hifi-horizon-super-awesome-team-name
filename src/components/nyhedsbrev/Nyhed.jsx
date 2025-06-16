a88712af0319b112060b61df44123eb112f77704
broto1234 <satyo97du@gmail.com>
2025-06-16 09:38:34 +0200

    Updated nyhed component

---
+++ src/components/nyhedsbrev/Nyhed.jsx
@@ -0,0 +1,16 @@
+
+export default function Nyhed() {
+
+  return (
+    <section className="nyhed">
+      {/* <div className="nyhed-content"> */}
+        <h3>SIGN UP FOR OUR NEWSLETTER</h3>
+        <p>Subscribing to our newsletter secures you up to date information about HiFi Horizons latest updates and offers.</p>
+        <form>
+          <input type="email" name="email" id="email" />
+          <button type="submit">Sign up</button>
+        </form>
+      {/* </div> */}
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero/Hero";
+import Nyhed from "../components/nyhedsbrev/Nyhed";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Nyhed />
         </>
     )
 }
