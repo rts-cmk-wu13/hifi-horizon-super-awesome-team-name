b174d9d7f498bac99d891280f0f340f332f7a373
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-16 12:36:19 +0200

    folder for shop item amount created

---
+++ src/components/productamount/ProductAmount.jsx
@@ -0,0 +1,22 @@
+import "./_ProductAmount.scss";
+
+export default function ProductAmount() {
+    // Function to handle the amount change when plus or minus is clicked
+    const handleAmountChange = (change) => {
+        // Logic to change the amount
+        // This could involve updating a state variable or calling a function
+        // to update the amount in the basket
+    };
+    // function to add the amount from handleAmountChange to the basket
+    const addToBasket = () => {
+        // Logic to add the current amount to the basket
+        // This could involve updating a state variable or calling a function
+        // to update the basket with the current amount
+    };
+
+    return (
+        <div className="product-amount">
+            
+        </div>
+    );
+}
