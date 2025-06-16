7706695228be581d58e26d4978061c87f10641d2
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-16 14:01:55 +0200

    counter added with add subtract function

--- src/components/productamount/ProductAmount.jsx
+++ src/components/productamount/ProductAmount.jsx
@@ -1,23 +1,27 @@
+import { useState } from "react";
 import "./_ProductAmount.scss";
-
+const initialCount = 0;
 export default function ProductAmount() {
-    // Function to handle the amount change when plus or minus is clicked
-    const handleAmountChange = (change) => {
-        // Logic to change the amount
-        // This could involve updating a state variable or calling a function
-        // to update the amount in the basket
-    };
-    // function to add the amount from handleAmountChange to the basket
-    const addToBasket = () => {
-        // Logic to add the current amount to the basket
-        // This could involve updating a state variable or calling a function
-        // to update the basket with the current amount
-    };
+    const [count, setCount] = useState(initialCount);
+
+    function handleSubtract() {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+    function handleAdd() {
+        setCount(count + 1);
+    }
 
     return (
         <div className="product__amount">
             <div className="product__colors">
-                <p>hej</p>
+
+            </div>
+            <div className="offer__amount">
+                <button type="button" class="subtractButton" onClick={handleSubtract}>-</button>
+                <div id="result">{count}</div>
+                <button type="button" class="addButton" onClick={handleAdd}>+</button>
             </div>
         </div>
     );
