22371a9c0ca547211a95d5bf955d0e11707d29bc
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-18 10:06:51 +0200

    page styling added for the contact-page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,15 @@
+import "../style/_Contact.scss";
 import { Link } from "react-router"
 import ContactFormula from "../components/contactformula/ContactFormula"
 
 export default function Contact() {
 
     return (
-        <>
+        <section className="contact">
             <h1>get in touch with us</h1>
             <ContactFormula />
-            <p>Visit our sister companies <Link to="/">Home Sound</Link> and The <Link to="/">Movie Rooms</Link> part of the HiFi Horizon Group. </p>
-        </>
+            <p className="contact__text">Visit our sister companies <Link to="/" className="contact__link">Home Sound</Link> and The <Link to="/" className="contact__link">Movie Rooms</Link> part of the HiFi Horizon Group. </p>
+        </section>
     )
 }
 
