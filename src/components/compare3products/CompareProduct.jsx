7ffcc2cb9e35aeb7c759d28157bb0f448ae44000
broto1234 <satyo97du@gmail.com>
2025-06-18 14:09:43 +0200

    updated compare compo

--- src/components/compare3products/CompareProduct.jsx
+++ src/components/compare3products/CompareProduct.jsx
@@ -9,7 +9,7 @@ export default function CompareProduct() {
         <img src={`/${product.image}`} alt={product.image} />
       </div>
       <div className="compare__right">
-        <h2>{product.categories}</h2>
+        <h4>{product.categories}</h4>
         <p>({product.brand})</p>
         <p className="compare--price">£{product.price}</p>
       </div>
