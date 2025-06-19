907526bd002b0ca684ea61f46a325e40f216b5c5
broto1234 <satyo97du@gmail.com>
2025-06-19 10:24:13 +0200

    updated product card

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Link, useLoaderData } from "react-router";
+import Compare from '../../components/compare/Compare'
 import './_productCard.scss';
 
 export default function ProductCard (){
     const products = useLoaderData();
 
     return (
-        <div className="products-page ">
+        <section className="products__card">
             {products.map(product => (
                 <div className="product-card" key={product.id}>
+                    <Compare className=""/>
                     <figure className='product-card__figure'>
                         <img className='product-card__img' src={product.image} alt="" />
                     </figure>
-                    <h3>{product.type}</h3>
-                    <p>{product.output}</p>
-                    <p>£{product.price}</p>
-                    <Link to={`/products/${product.id}`} className='product-card__btn'>Read more</Link>
+                    <p>{product.type}</p>
+                    <p>({product.output})</p>
+                    <p className="dolars--clr product--cardpara">£{product.price}</p>
+                    <div className="addStock">
+                        <Link to={`/products/${product.id}`} className='product-card__btn'>Read more</Link>
+                        <div className="stockCircle">
+                            <p>In stock</p>
+                            <span  className="stock--circle"></span>
+                        </div>
+
+                    </div>
                 </div>
             ))}
-        </div>
+        </section>
     );
 }
