b8f7c3647101c8661025d99c732fad4051e5192d
conztantin <conztantinsixten@gmail.com>
2025-06-19 13:22:01 +0200

    shopping cart and some styling added to the header to increse size. newsletter is also changed, but will need final styling in regards to mobile-vcersion

--- src/components/nyhedsbrev/Nyhed.jsx
+++ src/components/nyhedsbrev/Nyhed.jsx
@@ -2,13 +2,15 @@
 export default function Nyhed() {
 
   return (
-    <section className="nyhed">
+    <section className="">
+      <div className="nyhed">
       <h3>SIGN UP FOR OUR NEWSLETTER</h3>
       <p>Subscribing to our newsletter secures you up to date information about HiFi Horizons latest updates and offers.</p>
       <form>
         <input type="email" name="email" id="email" />
         <button type="submit" className="nyhed-btn">Sign up</button>
       </form>
+      </div>
     </section>
   )
 }
