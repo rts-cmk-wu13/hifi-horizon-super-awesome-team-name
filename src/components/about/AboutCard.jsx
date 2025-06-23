479dea7a884303189ecc90531575bf83f5aa92eb
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-23 11:04:05 +0200

    loader added for about page

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -1,19 +1,26 @@
+import { useLoaderData } from "react-router";
 import "./_AboutCard.scss";
 
-export default function AboutCard({ header, subheader, text, img, alt }) {
+export default function AboutCard({ alt }) {
+    const aboutContent = useLoaderData();
+
     return (
         <div className="about__card">
-            <img src={img} alt={alt} />
-            <section>
-                <h2>{header}</h2>
-                <h3>{subheader}</h3>
-                {Array.isArray(text) ?
-                    text.map((paragraph, index) => (
-                        <p key={index}>{paragraph}</p>
-                    )) :
-                    <p>{text}</p>
-                }
-            </section>
+            {aboutContent.map(about => (
+                <>
+                    <img src={about.image} alt={alt} />
+                    <section>
+                        <h2>{about.title}</h2>
+                        <h3>{about.catchfrase}</h3>
+                        {Array.isArray(about.text) ?
+                            about.text.map((paragraph, index) => (
+                                <p key={index}>{paragraph}</p>
+                            )) :
+                            <p>{about.text}</p>
+                        }
+                    </section>
+                </>
+            ))}
         </div>
     )
 }
