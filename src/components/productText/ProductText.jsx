1dac587c1815054109cf1ae56ddab8af7f07ac66
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-25 13:30:57 +0200

    product name changed

--- src/components/productText/ProductText.jsx
+++ src/components/productText/ProductText.jsx
@@ -4,7 +4,7 @@ export default function ProductText({ product }) {
   
   return (
     <div className="product__text">
-      <h2>{product.categories}</h2>
+      <h2>{product.type}</h2>
       <p>({product.brand})</p>
       <p className="product-description">{product.description}</p>
     </div>
--- src/utilities/typicode.js
+++ src/utilities/typicode.js
@@ -1,5 +1,5 @@
 
-import { redirect } from "react-router"
+// import { redirect } from "react-router"
 import queryClient from "./queryClient"
 
 // ! Uden tanstack:
